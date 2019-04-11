36217f50332697ebdc19daeff7eb546cf64c22f3
Chuk <chukmunnlee@gmail.com>
2019-04-11 15:46:36 +0800

    hello service to handle / for ingress

---
+++ hello/main.js
@@ -0,0 +1,16 @@
+const { join } = require('path')
+const express = require('express')
+
+const APP_PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
+
+const app = express();
+
+app.get('/health', (req, resp) => {
+	resp.status(200).type('text/plain').end(`${new Date()}: OK`);
+});
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.listen(APP_PORT, () => {
+	console.log('Application started on port %d at %s', APP_PORT, (new Date()).toString());
+});
